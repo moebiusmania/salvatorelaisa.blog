4681451f85ae6e6e3ceed486c57e1b7d18dd5c3d
Salvatore Laisa <moebiusmania@gmail.com>
2017-07-27 21:56:36 +0200

    reverted

--- index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-
-// or more concisely
-const sys = require('sys')
-const exec = require('child_process').exec;
-const puts = (error, stdout, stderr) => { sys.puts(stdout) }
-exec("mkdir ghost", puts);
-exec("cd ghost", puts);
