a646a5df7415a00b00718842ac748c7ed8a50be0
Salvatore Laisa <moebiusmania@gmail.com>
2021-06-30 11:39:55 +0200

    remove vuepress

--- src/.vuepress/theme/store/index.js
+++
@@ -1,8 +0,0 @@
-import Vuex from 'vuex'
-
-import state from './state'
-import actions from './actions'
-import mutations from './mutations'
-import getters from './getters'
-
-export default () => new Vuex.Store({ state, actions, mutations, getters })
