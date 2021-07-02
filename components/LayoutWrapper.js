ac0d52f07c4940aa6cd379c94cdda64c65419424
Salvatore Laisa <moebiusmania@gmail.com>
2021-07-02 09:35:06 +0200

    fix logo in mobile nav

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -15,7 +15,7 @@ const LayoutWrapper = ({ children }) => {
             <Link href="/" aria-label="Tailwind CSS Blog">
               <div className="flex items-center justify-between">
                 {typeof siteMetadata.headerTitle === 'string' ? (
-                  <div className="hidden h-6 text-2xl font-semibold sm:block">
+                  <div className="h-6 text-2xl font-semibold sm:block">
                     {siteMetadata.headerTitle}
                   </div>
                 ) : (
