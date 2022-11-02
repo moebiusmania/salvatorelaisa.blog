a5c53c980114614733cad35c07b56635de6d758a
moebiusmania <moebiusmania@gmail.com>
2022-11-02 16:04:16 +0100

    fix: add year filter page

--- pages/post/page/[page].vue
+++ pages/post/page/[page].vue
@@ -18,51 +18,33 @@ const posts: ParsedContent[] = await queryContent()
 <template>
   <div>
     <h1
-      class="text-3xl font-extrabold leading-9 tracking-tight text-base-content sm:text-4xl sm:leading-10 md:text-6xl md:leading-14"
-    >
+      class="text-3xl font-extrabold leading-9 tracking-tight text-base-content sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
       Tutti gli articoli
     </h1>
-    <input
+    <!-- <input
       type="text"
       placeholder="Cerca tra gli articoli"
       class="input input-bordered w-full md:max-w-xl my-10"
-    />
+    /> -->
     <ul class="my-8 divide-y divide-primary-content">
       <PostPreview v-for="post in posts" :post="post" />
     </ul>
     <div class="pt-6 pb-8 space-y-2 md:space-y-5">
       <nav class="flex justify-between">
-        <NuxtLink
-          v-if="prev > 0"
-          class="text-primary hover:text-primary-focus"
-          :href="`/post/page/${prev}`"
-        >
+        <NuxtLink v-if="prev > 0" class="text-primary hover:text-primary-focus" :href="`/post/page/${prev}`">
           <button aria-label="precedente">&larr; Precedente</button>
         </NuxtLink>
-        <button
-          aria-label="precedente"
-          v-else
-          disabled
-          class="text-neutral opacity-40"
-        >
+        <button aria-label="precedente" v-else disabled class="text-neutral opacity-40">
           &larr; Precedente
         </button>
 
         <span>Pagina {{ page }}</span>
 
-        <NuxtLink
-          v-if="posts.length === limit"
-          class="text-primary hover:text-primary-focus"
-          :href="`/post/page/${next}`"
-        >
+        <NuxtLink v-if="posts.length === limit" class="text-primary hover:text-primary-focus"
+          :href="`/post/page/${next}`">
           <button aria-label="successivo">Successivo &rarr;</button>
         </NuxtLink>
-        <button
-          aria-label="successivo"
-          v-else
-          disabled
-          class="text-neutral opacity-40"
-        >
+        <button aria-label="successivo" v-else disabled class="text-neutral opacity-40">
           Successivo &rarr;
         </button>
       </nav>
---
+++ pages/post/year/index.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+const BASE: number = 2010
+const CURRENT: number = new Date().getFullYear()
+const difference: number = (CURRENT - BASE) + 1
+const years: Array<string> = Array.from(new Array(difference), (e, i) => BASE + i).map((e) => e.toString())
+</script>
+
+<template>
+  <div
+    class="flex flex-col items-start justify-start divide-y divide-gray-200 dark:divide-gray-700 md:justify-center md:items-center md:divide-y-0 md:flex-row md:space-x-6 md:mt-24">
+    <div class="pt-6 pb-8 space-x-2 md:space-y-5">
+      <h1
+        class="text-3xl font-extrabold leading-9 tracking-tight text-base-content sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 capitalize">
+        Anni
+      </h1>
+    </div>
+    <div class="flex flex-wrap max-w-lg">
+      <div v-for="year in years" class="mt-2 mb-2 mr-5">
+        <NuxtLink class="mr-3 text-sm font-medium text-primary uppercase hover:primary-focus"
+          :href="`/post/year/${year}`">{{ year }}</NuxtLink>
+        <!-- <NuxtLink class="-ml-2 text-sm font-semibold text-gray-600 uppercase dark:text-gray-300"
+          :href="`/year/${tag.label}`">({{ tag.items }})</NuxtLink> -->
+      </div>
+    </div>
+  </div>
+</template>
