15a8a6cf9bf78d2763398f6bd3ff181481766021
moebiusmania <moebiusmania@gmail.com>
2023-04-15 12:45:33 +0200

    chore: deps update

--- utils/index.ts
+++ utils/index.ts
@@ -1,12 +1,12 @@
-
 type RawContent = <T>() => string;
 
 const KEY: string = "theme";
 
 export const LIGHT: string = "corporate";
-export const DARK: string = "halloween";
+export const DARK: string = "night";
 
-export const isDark = (): boolean => (localStorage.getItem(KEY) ? true : false);
+export const isDark = (): boolean =>
+  localStorage && localStorage.getItem(KEY) ? true : false;
 
 export const toggleDark = (value: boolean): void =>
-  localStorage.setItem(KEY, value ? DARK : LIGHT);
+  localStorage && localStorage.setItem(KEY, value ? DARK : LIGHT);
