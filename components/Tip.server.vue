e7292041c9d007588a5d17d3215fb7a0ce491fb9
moebiusmania <moebiusmania@gmail.com>
2023-06-16 14:33:21 +0200

    feat: try island feature on SSG

---
+++ components/Tip.server.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="!border-t-0 mx-auto">
+    <p class="mb-4 italic text-sm">
+      Se ti piace questo blog e ti va di supportarmi, o anche solo se se ti sto
+      un pò simpatico, puoi offrirmi un caffé! E' un piccolo gesto ma che
+      apprezzerei molto <span class="not-italic">🙂</span>.
+    </p>
+    <div class="md:w-max mx-auto">
+      <Coffee />
+    </div>
+  </section>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ["@nuxt/content", "@nuxtjs/tailwindcss"],
+  experimental: {
+    componentIslands: true
+  },
   nitro: {
     prerender: {
       routes: ['/sitemap.xml', '/rss.xml']
