46c6c818e0ef9362b5806c4d0fe38bd77f90b8e7
moebiusmania <moebiusmania@gmail.com>
2023-10-01 12:51:31 +0200

    feat: adding an "outdated" warning for 2+ years old posts

---
+++ components/Outdated.server.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+interface Props {
+  date: string;
+}
+
+const { date = "" } = defineProps<Props>();
+
+const postYear: number = new Date(date).getFullYear();
+const current: number = new Date().getFullYear();
+
+const isOutdated: boolean = current - postYear > 2;
+console.log(postYear, current, isOutdated);
+</script>
+
+<template>
+  <div class="alert shadow-lg my-10" v-if="isOutdated">
+    <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+        d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+    </svg>
+    <span><strong>ATTENZIONE!</strong> Questo post ha qualche anno sulle spalle, quindi è molto probabile che il contesto
+      degli argomenti
+      trattati non sia più attuale. Controlla la data prima di continuare a leggere 🙂</span>
+  </div>
+</template>
