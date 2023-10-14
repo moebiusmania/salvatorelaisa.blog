df192fcb658cdddf2cba63d94c7d5adba04d9555
moebiusmania <moebiusmania@gmail.com>
2023-10-14 11:20:59 +0200

    feature: dark mode working as intended

--- middleware/theme.global.ts
+++ middleware/theme.global.ts
@@ -1,10 +1,4 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-  console.log("----");
-  console.log("from.path", from.path);
-  console.log("from.query", from.query);
-  console.log("to.path", to.path);
-  console.log("to.query", to.query);
-
   if (Object.keys(from.query).length && !Object.keys(to.query).length) {
     return navigateTo({
       path: to.path,
@@ -13,11 +7,4 @@ export default defineNuxtRouteMiddleware((to, from) => {
       query: from.query,
     });
   }
-
-  // if (from.query.theme === "dark" && from.path !== to.path) {
-  //   //return to.path + "?theme=dark";
-  //   return navigateTo(to.path + "?theme=dark");
-  // } else {
-  //   return true;
-  // }
 });
