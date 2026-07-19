eda844e218ddd1d16355ea75d5c517210b9276c0
Salvatore Laisa <salvatore.laisa@carol.health>
2026-07-19 18:12:34 +0200

    fix: icon colors to match theme

--- app/components/icons/External.server.vue
+++ app/components/icons/External.server.vue
@@ -1,5 +1,5 @@
 <template>
-  <svg fill="#000000" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+  <svg fill="currentColor" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
     <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
     <g
       id="SVGRepo_tracerCarrier"
